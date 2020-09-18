1cf14b202442f3c41c170cb4f53927e47e05da9e
alfonso-dev <alfonso.olavarria@indagia.com>
2020-09-18 13:56:56 -0400

    plantilla

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
-const express = require('express')
-const app = express()
-const port = 3000
-var bodyParser = require('body-parser')
+const express = require('express');
+var path = require('path');
+const port = 3000;
+var bodyParser = require('body-parser');
 var mainjs = require('./router/main.js');
 var apiscrap = require('./router/apiscrap.js');
+
+const app = express();
+
+
+// view engine setup
+app.set('view engine', 'ejs');
+//setup public folder
+app.use(express.static('./public'));
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello World AE!')
+  res.render('eiser/index',{
+    links:{"a":"A"}
+  });
+  // res.send('Hello World AE!')
 })
 
 app.use('/main', mainjs);
